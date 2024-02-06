d1b0fef4fc7298255f4f234c74f3e4e673240d28
Thiago Rocha <mrthiago09@gmail.com>
2024-02-06 20:15:02 -0300

    Adicao das partes esteticas projeto FocusTimer

---
+++ Projeto10-llFocusTimer/src/FocusTimer/elements.js
@@ -0,0 +1,11 @@
+export const controls = document.querySelector(".controls");
+
+export const natureSound = document.querySelector("#nature");
+
+export const rainSound = document.querySelector("#rain");
+
+export const marketSound = document.querySelector("#market");
+
+export const campFire = document.querySelector("#camp-fire");
+
+export const minutes = document.querySelector("#minutes");
